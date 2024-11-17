13959ec0322f85b33c2817b83a3afcac616e03a6
Nikita Lutsko <nikitalutsko75@gmail.com>
2024-11-17 13:34:04 +0100

    todo-js

--- src/App.js
+++ src/App.js
@@ -2,42 +2,63 @@ import { useState } from "react";
 import "./style/style.css";
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [checked, setChecked] = useState([]);
+  const [inputValue, setInputValue] = useState([]);
+
+  const addTodo = () => {
+    if (inputValue.trim !== "") {
+      setTodo([...todo, inputValue]);
+      setChecked([...checked, false]);
+      setInputValue("");
+    }
+  };
+
+  const removeTodo = (index) => {
+    const newTodo = todo.filter((_, i) => i !== index);
+    setTodo(newTodo);
+    const newCheck = checked.filter((_, i) => i !== index);
+    setChecked(newCheck);
+  };
+
+  const checkChange = (index) => {
+    const updateCheck = checked.map((state, i) =>
+      i === index ? !state : state
+    );
+    setChecked(updateCheck);
+  };
+
   return (
-    <div class="App">
-      <div class="task-manager">
-        <div class="task-input">
-          <input type="text" placeholder="Create your task here..." />
-          <button>Add</button>
+    <div className="App">
+      <div className="task-manager">
+        <div className="task-input">
+          <input
+            type="text"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            placeholder="Create your task here..."
+          />
+          <button onClick={addTodo}>Add</button>
         </div>
-        <div class="tasks">
-          <div class="task">
-            <label>
-              <input type="checkbox" />
-              <span class="task-text">Assign task to john wright</span>
-            </label>
-            <button class="delete-btn">🗑</button>
-          </div>
-          <div class="task">
-            <label>
-              <input type="checkbox" />
-              <span class="task-text">Complete Social media posts</span>
-            </label>
-            <button class="delete-btn">🗑</button>
-          </div>
-          <div class="task">
-            <label>
-              <input type="checkbox" />
-              <span class="task-text">Upload documents</span>
-            </label>
-            <button class="delete-btn">🗑</button>
-          </div>
-          <div class="task">
-            <label>
-              <input type="checkbox" value="" />
-              <span class="task-text">Recharge broadband bill</span>
-            </label>
-            <button class="delete-btn">🗑</button>
-          </div>
+        <div className="tasks">
+          {todo.map((text, index) => (
+            <div
+              key={index}
+              className={`task ${checked[index] ? "completed" : ""}`}
+            >
+              <label>
+                <input
+                  type="checkbox"
+                  onClick={() => checkChange(index)}
+                  checked={checked[index]}
+                />
+                <span className="task-text">{text}</span>
+              </label>
+              <button onClick={() => removeTodo(index)} className="delete-btn">
+                🗑
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
