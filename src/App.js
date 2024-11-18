41a3b5e4844dafa6d71fdbb16d1e5f2235eae850
Nikita Lutsko <nikitalutsko75@gmail.com>
2024-11-18 19:34:58 +0100

    get-api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import "./style/style.css";
 import { Block } from "./Block";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [fromCurrency, setFromCurrency] = useState("RUB");
+  const [toCurrency, setToCurrency] = useState("EUR");
+  const [rates, setRates] = useState({});
+
+  useEffect(() => {
+    fetch("https://open.er-api.com/v6/latest/USD")
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json.rates);
+        console.log(json.rates);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Failed to retrieve information");
+      });
+  }, []);
+
+  const onChangeFromPrice = (value) => {};
+
   return (
     <div className="App">
       <Block
         value={0}
-        currency="RUB"
-        onChangeCurrency={(cur) => console.log(cur)}
+        currency={fromCurrency}
+        onChangeCurrency={setFromCurrency}
       />
-      <Block value={0} currency="USD" />
+      <Block value={0} currency={toCurrency} onChangeCurrency={setToCurrency} />
     </div>
   );
 }
