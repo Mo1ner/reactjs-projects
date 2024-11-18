147653dcb9dc9b0bd28d222280360894ce62d4f6
Nikita Lutsko <nikitalutsko75@gmail.com>
2024-11-18 19:49:44 +0100

    add-converter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { useEffect, useState } from "react";
 function App() {
   const [fromCurrency, setFromCurrency] = useState("RUB");
   const [toCurrency, setToCurrency] = useState("EUR");
+  const [fromPrice, setFromPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(0);
+
   const [rates, setRates] = useState({});
 
   useEffect(() => {
@@ -20,16 +23,35 @@ function App() {
       });
   }, []);
 
-  const onChangeFromPrice = (value) => {};
+  const onChangeFromPrice = (value) => {
+    const price = value / rates[fromCurrency];
+    const result = price * rates[toCurrency];
+
+    setToPrice(result);
+    setFromPrice(value);
+  };
+
+  const onChangeToPrice = (value) => {
+    const result = (rates[fromCurrency] / rates[toCurrency]) * value;
+
+    setFromPrice(result);
+    setToPrice(value);
+  };
 
   return (
     <div className="App">
       <Block
-        value={0}
+        value={fromPrice}
         currency={fromCurrency}
         onChangeCurrency={setFromCurrency}
+        onChangeValue={onChangeFromPrice}
       />
-      <Block value={0} currency={toCurrency} onChangeCurrency={setToCurrency} />
+      <Block
+        value={toPrice}
+        currency={toCurrency}
+        onChangeCurrency={setToCurrency}
+        onChangeValue={onChangeToPrice}
+      />
     </div>
   );
 }
