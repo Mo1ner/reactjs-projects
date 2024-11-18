e66e03f9a351c7f4667933657419ba12365ce799
Nikita Lutsko <nikitalutsko75@gmail.com>
2024-11-18 20:06:01 +0100

    async-update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import "./style/style.css";
 import { Block } from "./Block";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function App() {
   const [fromCurrency, setFromCurrency] = useState("RUB");
-  const [toCurrency, setToCurrency] = useState("EUR");
+  const [toCurrency, setToCurrency] = useState("USD");
   const [fromPrice, setFromPrice] = useState(0);
-  const [toPrice, setToPrice] = useState(0);
+  const [toPrice, setToPrice] = useState(1);
 
-  const [rates, setRates] = useState({});
+  const ratesRef = useRef({});
 
   useEffect(() => {
     fetch("https://open.er-api.com/v6/latest/USD")
       .then((res) => res.json())
       .then((json) => {
-        setRates(json.rates);
-        console.log(json.rates);
+        ratesRef.current = json.rates;
+        onChangeToPrice(1);
       })
       .catch((err) => {
         console.warn(err);
@@ -24,19 +24,28 @@ function App() {
   }, []);
 
   const onChangeFromPrice = (value) => {
-    const price = value / rates[fromCurrency];
-    const result = price * rates[toCurrency];
+    const price = value / ratesRef.current[fromCurrency];
+    const result = price * ratesRef.current[toCurrency];
 
     setToPrice(result);
     setFromPrice(value);
   };
 
   const onChangeToPrice = (value) => {
-    const result = (rates[fromCurrency] / rates[toCurrency]) * value;
+    const result =
+      (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
 
     setFromPrice(result);
     setToPrice(value);
   };
+
+  useEffect(() => {
+    onChangeFromPrice(fromPrice);
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    onChangeToPrice(toPrice);
+  }, [toCurrency]);
 
   return (
     <div className="App">
