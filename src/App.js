0c646e7b95855d48a9ac7a917646bb321b572b4a
Nikita Lutsko <nikitalutsko75@gmail.com>
2024-11-27 18:20:47 +0100

    init-carousel

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Item from "./comp/Item";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Item />
+    </div>
+  );
 }
 
 export default App;
--- src/comp/Item.jsx
+++ src/comp/Item.jsx
@@ -1,18 +1,65 @@
-import React from "react";
-import img from "./../img/macbook-pro 1.png";
+import React, { useState } from "react";
+import jacket from "./../img/jacket.png";
+import sweater from "./../img/sweater.png";
+import sweater1 from "./../img/sweater-1.png";
+import tShirt from "./../img/t-shirt.png";
 
-const data = {
-  img1: { img },
-  img2: { img },
-  img3: { img },
-  img4: { img },
-};
+import "./item.css";
 
 const Item = () => {
+  const images = [jacket, sweater, sweater1, tShirt];
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleArrowClick = (direction) => {
+    if (direction === "prev") {
+      setActiveIndex((prevIndex) =>
+        prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      );
+    } else if (direction === "next") {
+      setActiveIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    }
+  };
+
+  const handleMouseEnter = (index) => {
+    setActiveIndex(index);
+  };
+
   return (
-    <div className="item">
+    <div className="carousel">
       <div className="container">
-        <div className="item-inner"></div>
+        <div className="carousel-inner">
+          <div className="main-img">
+            <img src={images[activeIndex]} alt={`Slide ${activeIndex}`} />
+
+            <div className="carousel-btn">
+              <button
+                className="left-btn"
+                onClick={() => handleArrowClick("prev")}
+              >
+                ←
+              </button>
+              <button
+                className="right-btn"
+                onClick={() => handleArrowClick("next")}
+              >
+                →
+              </button>
+            </div>
+          </div>
+          <div className="carousel-img">
+            {images.map((item, index) => (
+              <img
+                key={index}
+                src={item}
+                alt={item}
+                onMouseEnter={() => handleMouseEnter(index)}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
