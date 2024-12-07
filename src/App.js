e2a5f3bde1f40f2b77cfaeaa23dcbc0399b77930
Nikita Lutsko <nikitalutsko75@gmail.com>
2024-12-07 12:00:30 +0100

    add-3d

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const earthModel = useGLTF("earth.gltf");
+
+  return (
+    <>
+      <Canvas>
+        <ambientLight />
+        <OrbitControls
+          minDistance={2}
+          maxPolarAngle={5}
+          zoomSpeed={1}
+          enableDamping={true}
+          dampingFactor={0.1}
+        />
+        <Suspense fallback={null}>
+          <primitive object={earthModel.scene} />
+        </Suspense>
+        <Environment preset="sunset" />
+      </Canvas>
+    </>
+  );
 }
 
 export default App;
